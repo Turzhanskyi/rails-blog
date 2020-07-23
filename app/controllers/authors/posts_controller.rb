f9641a38f9191395b3beb1cde6955f7e09c41bde
Turzhanskyi <turzhansky81@gmail.com>
2020-07-23 11:21:27 +0300

    Add header_image to Post form

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -5,7 +5,6 @@ module Authors
     before_action :set_post, only: %i[edit update destroy]
 
     # GET /posts
-    # GET /posts.json
     def index
       @posts = current_author.posts
     end
@@ -21,43 +20,29 @@ module Authors
     end
 
     # POST /posts
-    # POST /posts.json
     def create
       @post = current_author.posts.build(post_params)
 
-      respond_to do |format|
-        if @post.save
-          format.html { redirect_to @post, notice: 'Post was successfully created.' }
-          format.json { render :show, status: :created, location: @post }
-        else
-          format.html { render :new }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
+      if @post.save
+        redirect_to edit_post_path(@post)
+      else
+        render :new
       end
     end
 
     # PATCH/PUT /posts/1
-    # PATCH/PUT /posts/1.json
     def update
-      respond_to do |format|
-        if @post.update(post_params)
-          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-          format.json { render :show, status: :ok, location: @post }
-        else
-          format.html { render :edit }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
+      if @post.update(post_params)
+        redirect_to edit_post_path(@post)
+      else
+        render :edit
       end
     end
 
     # DELETE /posts/1
-    # DELETE /posts/1.json
     def destroy
       @post.destroy
-      respond_to do |format|
-        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+      redirect_to posts_url, notice: 'Post was successfully destroyed.'
     end
 
     private
@@ -69,7 +54,7 @@ module Authors
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :header_image)
     end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@
 class Post < ApplicationRecord
   belongs_to :author
   has_many :elements, dependent: :destroy
+
+  has_one_attached :header_image
 end
